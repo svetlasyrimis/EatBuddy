4873e09505a943dab7e6f8393ba15b89e5170bb3
Svetla <svetla.syrimis@gmail.com>
2019-07-23 09:41:37 -0400

    SS front end bugs, login and register work

--- client/src/components/Shuffler.jsx
+++ client/src/components/Shuffler.jsx
@@ -1,59 +1,34 @@
-import React from 'react'
-
-import CrossfadeImage from 'react-crossfade-image'
-
-// import Container from 'react-bootstrap/Container'
-// import Col from 'react-bootstrap/Col'
-// import Row from 'react-bootstrap/Row'
-// import Button from 'react-bootstrap/Button'
-import { Row, Col, Container } from 'react-bootstrap'
+import React from 'react';
+import CrossfadeImage from 'react-crossfade-image';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 
-
-const CardCombo = (props) => {
-  const { food, foodImage, drink, drinkImage } = props.data
-
-
-
+const Shuffler = (props) => {
+  const { food, foodImage, drink, drinkImage } = props.data;
   return (
-
     <>
-
       <Container >
-
-        {/* <h3>{food} & {drink} </h3> */}
         <Row>
           <Col md={{ span: 3, offset: 3 }}>
-
             <CrossfadeImage src={foodImage} style={{ maxWidth: "100%", borderRadius: "20%" }}
               timingFunction={"ease-in-out"}
               duration={800}
               alt="food" />
             <p>{food}</p>
           </Col>
-
           <Col md={{ span: 3, offset: -3 }}>
             <CrossfadeImage src={drinkImage} style={{ maxWidth: "100%", borderRadius: "20%" }}
               timingFunction={"ease-in-out"}
-
               duration={800}
               alt='dr' />
             <p>{drink}</p>
           </Col>
         </Row>
-
       </Container >
-
-
     </>
-
-
-
-
-
-
-
   )
 }
 
-export default CardCombo
+export default Shuffler

f9590762051242101126c7db423231d602500e96
Riti Sharma <riti.sharma11@yahoo.com>
2019-07-24 15:33:31 -0400

    rs conditional rendering on board

--- client/src/components/ComboBoard.jsx
+++ client/src/components/ComboBoard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Shuffler from './Shuffler';
 import Nav from './Nav';
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 import MakeCombo from './MakeCombo'
 
 const ComboBoard = (props) => {
@@ -10,22 +10,12 @@ const ComboBoard = (props) => {
       <Nav
         handleLogout={props.handleLogout}
       />
-      {/* {props.combos.length > 0 ?  :
-        <Route path="/home" render={() => (
-          <>
-
-            <p>Hello {this.state.currentUser.name}!</p>
-            <MakeCombo
-              isLoggedIn={this.state.isLoggedIn}
-              currentView={this.state.currentView}
-              loginFormData={this.state.loginFormData}
-              handleLogout={this.handleLogout}
-              meal={this.state.meal}
-              fetchMealDrink={this.fetchMealDrink}
-              changeBoard={this.changeBoard}
-            />
-          </>
-        )} />} */}
+      {props.combos.length > 0 ?
+        <Redirect to="/combo" />
+        :
+        <>
+          <Redirect to="/home" />
+        </>}
       {props.combos && props.combos.map(combo => (
         <div key={combo.id}>
           <button name={combo.id} onClick={props.handleComboDelete}>Delete</button>

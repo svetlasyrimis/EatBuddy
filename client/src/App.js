7592a93732dfd3f5abe0c08d4ceb4b04a0522b2a
Svetla <svetla.syrimis@gmail.com>
2019-09-06 22:21:14 -0400

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -232,7 +232,7 @@ class App extends React.Component {
     
     
     // https://mealmatchpandas.herokuapp.com/combos/
-    const resp = await axios.put(`http://localhost:3005/combos/${comboId}`, this.state.currentCombo);
+    const resp = await axios.put(`https://mealmatch123.herokuapp.com/combos/${comboId}`, this.state.currentCombo);
     const favorite = resp.data;
     this.setState(prevState => ({
       favorites: [favorite, ...prevState.favorites]
